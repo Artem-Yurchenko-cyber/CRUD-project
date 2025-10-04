c06aff8577c0b075e5b4f010c50dd5395f050736
Artem Yurchenko <yurchenko.artem13@gmail.com>
2025-10-04 15:38:12 +0300

    chenged structure

--- app.py
+++ app.py
@@ -2,48 +2,58 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Дані зберігаються у пам'яті
 books = [
     {"id": 1, "title": "Мистецтво війни", "author": "Сунь-цзи", "pages": 180, "status": "Прочитано"},
     {"id": 2, "title": "1984", "author": "Джордж Орвелл", "pages": 328, "status": "Не прочитано"},
 ]
+# next_id = 3
+next_id = max(b["id"] for b in books) + 1 if books else 1
 
 @app.route("/")
 def index():
     return render_template("index.html", books=books)
 
-# Create
 @app.route("/add", methods=["POST"])
 def add_book():
-    new_id = max([b["id"] for b in books]) + 1 if books else 1
+    global next_id
     title = request.form["title"]
     author = request.form["author"]
     pages = request.form["pages"]
     status = request.form["status"]
-    books.append({"id": new_id, "title": title, "author": author, "pages": pages, "status": status})
+
+    books.append({
+        "id": next_id,
+        "title": title,
+        "author": author,
+        "pages": pages,
+        "status": status
+    })
+    next_id += 1
+
     return redirect(url_for("index"))
 
-# Update
-@app.route("/edit/<int:book_id>", methods=["GET", "POST"])
-def edit_book(book_id):
-    book = next((b for b in books if b["id"] == book_id), None)
-    if not book:
-        return "Book not found", 404
+@app.route("/delete/<int:id>")
+def delete_book(id):
+    global books
+    books = [b for b in books if b["id"] != id]
+    return redirect(url_for("index"))
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_book(id):
+    book = next((b for b in books if b["id"] == id), None)
 
     if request.method == "POST":
-        book["title"] = request.form["title"]
-        book["author"] = request.form["author"]
-        book["pages"] = request.form["pages"]
-        book["status"] = request.form["status"]
+        if book:
+            book["title"] = request.form["title"]
+            book["author"] = request.form["author"]
+            book["pages"] = request.form["pages"]
+            book["status"] = request.form["status"]
         return redirect(url_for("index"))
 
     return render_template("edit.html", book=book)
 
-# Delete
-@app.route("/delete/<int:book_id>", methods=["POST"])
-def delete_book(book_id):
-    global books
-    books = [b for b in books if b["id"] != book_id]
-    return redirect(url_for("index"))
-
+# Запуск сервера
 if __name__ == "__main__":
+    # debug = True -> сервер перехапускає
     app.run(debug=True)
